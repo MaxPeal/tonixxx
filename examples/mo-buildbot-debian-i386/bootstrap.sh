75a50d20cff73a1279128e10ff97ce81a311292f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-05-17 21:22:44 -0500

    close #68

---
+++ examples/mo-buildbot-debian-i386/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cabal update &&
+    cabal install happy &&
+    cabal install hlint &&
+    cabal install shake
---
+++ examples/mo-buildbot-windows-amd64/bootstrap.ps1
@@ -0,0 +1,4 @@
+cabal update
+cabal install happy
+cabal install hlint
+cabal install shake
