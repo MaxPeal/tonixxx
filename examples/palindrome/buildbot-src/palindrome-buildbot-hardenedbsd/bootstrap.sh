058b96afe68a5a329bc9425ff0d352dc93dd3e19
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-02-29 17:39:21 -0600

    update hardenedbsd

--- examples/palindrome/buildbot-src/palindrome-buildbot-hardenedbsd/bootstrap.sh
+++ examples/palindrome/buildbot-src/palindrome-buildbot-hardenedbsd/bootstrap.sh
@@ -4,12 +4,9 @@ sudo hbsd-update &&
     sudo pkg install -y \
         cmake \
         cppcheck \
-        vera++ \
         valgrind \
         python3 \
         ca_root_nss &&
-    cp -r /usr/local/lib/vera++ /home/vagrant/.vera++ &&
-    ln -sf /vagrant/vera /home/vagrant/.vera++/profiles/default &&
     sudo python3 -m ensurepip &&
     sudo pip3 install --upgrade pip &&
     sudo pip3 install cpplint &&
