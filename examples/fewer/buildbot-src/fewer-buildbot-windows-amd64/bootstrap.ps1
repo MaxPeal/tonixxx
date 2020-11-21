0a4d7204c7f00359508393a2b3e6c9b25bbb0128
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-11-21 15:28:09 -0600

    sketch out new formulas (untested)

--- examples/fewer/buildbot-src/fewer-buildbot-windows-amd64/bootstrap.ps1
+++ examples/fewer/buildbot-src/fewer-buildbot-windows-amd64/bootstrap.ps1
@@ -1,22 +1,35 @@
 $ErrorActionPreference = "Stop"
 
+#
 # Install cmake, add to session PATH, persist PATH.
+#
 choco install -force -y cmake
 $cmakePath = "C:\Program Files\CMake\bin"
 $env:PATH += ";$cmakePath"
 $userPath = (Get-ItemProperty "Registry::HKEY_CURRENT_USER\Environment").PATH
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Environment" -Name PATH -Value "$userPath;$cmakePath"
 
+#
 # Install cppcheck, add to session PATH, persist PATH.
+#
 choco install -force -y cppcheck
 $cppcheckPath = "C:\Program Files\Cppcheck"
 $env:PATH += ";$cppcheckPath"
 $userPath = (Get-ItemProperty "Registry::HKEY_CURRENT_USER\Environment").PATH
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Environment" -Name PATH -Value "$userPath;$cppcheckPath"
 
+#
+# Install doxygen + graphviz
+#
+choco install -force -y doxygen.portable
+choco install -force -y graphviz.portable
+
 # Update PowerShell SSL protocols
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
+#
+# Install Python dependencies.
+#
 choco install -force -y python3
 $pythonPath = "C:\Python39"
 $pythonScriptPath = "C:\Python39\Scripts"
@@ -24,4 +37,4 @@ $env:PATH += ";$pythonPath;$pythonScriptPath"
 $userPath = (Get-ItemProperty "Registry::HKEY_CURRENT_USER\Environment").PATH
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Environment" -Name PATH -Value "$userPath;$pythonPath;$pythonScriptPath"
 python -m pip install -U pip
-pip3 install cpplint
+pip3 install -r C:\vagrant\requirements-dev.txt
