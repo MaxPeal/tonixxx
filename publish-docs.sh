a0bb5e902538b72ce7028fb0926154472037cfd3
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-12-05 15:00:09 -0600

    travis

--- publish-docs.sh
+++ publish-docs.sh
@@ -3,22 +3,13 @@ unset IFS
 set -euf
 
 DOCS="${TRAVIS_BUILD_DIR}/docs"
-
-echo "DOCS: $DOCS"
-
 PAGE_REPO="${DOCS}/_pages"
-
-echo "PAGE_REPO: $PAGE_REPO"
 
 mkdir "$DOCS"
 
 HOST_PATH="github.com/${TRAVIS_REPO_SLUG}.git"
 
-echo "HOST_PATH: $HOST_PATH"
-
 git clone -b gh-pages "https://git@${HOST_PATH}" "$PAGE_REPO"
-
-echo "CLONED PAGES"
 
 cd "$PAGE_REPO"
 rm -rf *
@@ -26,24 +17,14 @@ cd "$TRAVIS_BUILD_DIR"
 
 DOXYGEN_FILES="$(find . -name Doxyfile)"
 
-echo "DOXYGEN_FILES: $DOXYGEN_FILES"
-
 for DOXYGEN_FILE in $DOXYGEN_FILES; do
-    PROJECT="$(readlink -f "$(dirname -- "$DOXYGEN_FILE")")"
-
-    echo "PROJECT: $PROJECT"
+    PROJECT_BASE="$(dirname -- "$DOXYGEN_FILE")"
+    PROJECT="$(readlink -f "$PROJECT_BASE")"
 
     cd "${PROJECT}"
-
     doxygen Doxyfile
-
-    echo "GENERATED DOCS"
-
-    mkdir -p "${PAGE_REPO}/${PROJECT}"
-    cp -r html "${PAGE_REPO}/${PROJECT}"
-
-    echo "COPIED ARTIFACTS"
-
+    mkdir -p "${PAGE_REPO}/${PROJECT_BASE}"
+    cp -r html "${PAGE_REPO}/${PROJECT_BASE}"
     cd "$TRAVIS_BUILD_DIR"
 done
 
@@ -53,7 +34,4 @@ git config user.name 'TravisCI'
 git config user.email 'travis@travis-ci.org'
 git add --force --all
 git commit -m 'doxygen'
-
-echo "GENERATED COMMIT"
-
 git push -f "https://${GH_REPO_TOKEN}@${HOST_PATH}" 2>&1 >/dev/null
