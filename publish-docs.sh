31c35b30255d82f270ba89d5a83646559497169c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-12-05 12:50:36 -0600

    travis

---
+++ publish-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -euf
+unset IFS
+
+mkdir docs
+cd docs
+
+HOST_PATH="github.com/${TRAVIS_REPO_SLUG}.git"
+
+git clone -b gh-pages "https://git@${HOSTPATH}"
+cd "$GH_REPO_NAME"
+
+git config --global push.default simple
+git config user.name "TravisCI"
+git config user.email "travis@travis-ci.org"
+
+rm -rf *
+
+find . -name Doxyfile -exec doxygen "{}" \;
+
+git add -a
+git commit -m 'doxygen'
+git push -f "https://${GH_REPO_TOKEN}@${HOST_PATH}" 2>&1 >/dev/null
