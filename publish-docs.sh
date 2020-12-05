cb52c6c7066d017710eff96fd9b8f17840709ac6
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-12-05 13:34:47 -0600

    travis

--- publish-docs.sh
+++ publish-docs.sh
@@ -2,27 +2,28 @@
 set -euf
 unset IFS
 
-mkdir docs
-cd docs
+DOCS='docs'
+mkdir "$DOCS"
 
 HOST_PATH="github.com/${TRAVIS_REPO_SLUG}.git"
+REPO="${TRAVIS_REPO_SLUG#*/}"
 
-git clone -b gh-pages "https://${GH_REPO_TOKEN}@${HOST_PATH}"
-cd "${TRAVIS_REPO_SLUG#*/}"
+sh -c "cd \"\$DOCS\" && git clone -b gh-pages \"https://\${GH_REPO_TOKEN}@\${HOST_PATH}\" && cd \"\$REPO\" && rm -rf *"
+
+DOXYGEN_FILES="$(find . -name Doxyfile)"
+
+for DOXYGEN_FILE in $DOXYGEN_FILES; do
+    doxygen "$DOXYGEN_FILE"
+
+    PROJECT="$(dirname -- "$DOXYGEN_FILE")"
+
+    mkdir -p "${DOCS}/${REPO}/${PROJECT}"
+    cp -r "${PROJECT}/html" "${DOCS}/${REPO}/${PROJECT}"
+done
 
 git config --global push.default simple
 git config user.name "TravisCI"
 git config user.email "travis@travis-ci.org"
-
-rm -rf *
-
-find . -name Doxyfile -exec doxygen "{}" \;
-
-if [ -z "$(find . -name html | grep .)" ]; then
-    echo "missing doxygen artifacts"
-    exit 1
-else
-    git add --all
-    git commit -m 'doxygen'
-    git push -f "https://${GH_REPO_TOKEN}@${HOST_PATH}" 2>&1 >/dev/null
-fi
+git add --force --all
+git commit -m 'doxygen'
+git push -f "https://${GH_REPO_TOKEN}@${HOST_PATH}" 2>&1 >/dev/null
