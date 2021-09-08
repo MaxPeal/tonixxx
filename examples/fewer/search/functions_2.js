35a390d4ba2bb2d082147abe949655da13256514
TravisCI <travis@travis-ci.org>
2021-09-08 18:50:12 +0000

    Travis build: 51
    Commit: ce00d6d35ad0ffc2abccac958774e3efaf1d5b7c

--- examples/fewer/search/functions_2.js
+++ examples/fewer/search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['render_5fboi_11',['render_boi',['../fewer_8h.html#a0394fd76698db06792c40dd56e7828c9',1,'render_boi(char *hexpair, unsigned char c):&#160;fewer.c'],['../fewer_8c.html#a0394fd76698db06792c40dd56e7828c9',1,'render_boi(char *hexpair, unsigned char c):&#160;fewer.c']]]
+  ['render_5fboi_11',['render_boi',['../fewer_8h.html#a7e70bab3f533afb7a86574edac155153',1,'render_boi(char *hexpair, unsigned char c):&#160;fewer.c'],['../fewer_8c.html#a7e70bab3f533afb7a86574edac155153',1,'render_boi(char *hexpair, unsigned char c):&#160;fewer.c']]]
 ];
